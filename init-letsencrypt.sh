5fe979fd80a396ffb0cf830cb2f6c0eca1dabfcf
aitlp <aitlpmw@gmail.com>
2020-03-20 22:54:30 +0800

    all done

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -8,7 +8,7 @@ fi
 domains=(your_domain)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="you_email_address" # Adding a valid address is strongly recommended
+email="your_email_address" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
